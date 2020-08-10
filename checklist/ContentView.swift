58ac2209c5ce7ea6e8797a93145fa1805b374610
Róbert Konczi <robert.konczi@eman.cz>
2020-08-10 17:27:48 +0200

    Initial Commit

---
+++ checklist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  checklist
+//
+//  Created by Róbert Konczi on 10/08/2020.
+//  Copyright © 2020 Róbert Konczi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
